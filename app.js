161f5a22f6c3035963ea8c51c4a78c78b71db6f0
Saad Ali <64375942+saadkk05@users.noreply.github.com>
2020-06-21 01:40:21 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,211 @@
+
+
+CHAPTER NO 20 TO 24
+
+
+
+(1) Task No One. 
+
+// function name(a)
+// {
+// var a=prompt("Enter Your First Name.")
+// var b=prompt("Enter Your Last Name.")
+// alert("Full Name Is " + a + b )
+// }
+// name();
+
+
+ (2) Task No Two.
+
+
+// var a=prompt("Enter Your Favourate Mobile Name")
+// document.write(a.length);
+
+
+(3) Task No Three.
+
+// var a=("Pakistani")
+// document.write(a.indexOf("n"))
+
+
+(4) Task No Four.
+
+// var a=("Hello world");
+// var b =(a.lastIndexOf("l"));
+// document.write(b)
+
+
+(5) Task No Five.
+
+// var a=("pakistani")
+// var b =(a.indexOf("i"))
+// document.write(b)
+
+
+(6) Task No Six.
+
+// var a = ("Saad ");
+// var b = ("Ali");
+// var c = (a+b);
+// document.write(c);
+
+
+
+(7) Task No Seven.
+
+// var a = ("Hyderabad")
+// document.write(a.replace("Hyder","Islam"));
+
+
+(8) Task No Eight.
+
+// var a = ("Ali and Sami are best friends. They play cricket and football together.");
+// document.write(a.replace(/and/g,"&"));
+
+(10) Task No Ten
+
+// var a = prompt("Enter The Word To Change In Uppercase.")
+// document.write(a.toUpperCase())
+
+
+(11) Task No Eleven.
+
+// var a = prompt("Enter Word To Capatalise.")
+// var a = a.charAt(0).toUpperCase() + a.slice(1)
+// document.write(a)
+
+
+(!2) Task No Tweleve
+
+// var num = 35.36;
+// var a = Math.round(num)
+// document.write(a)
+
+
+
+
+(13) Task No Thirteen
+
+// var a = prompt("Enter Your User Name")
+// if(a === !,@,$,#,%,^,&,*){
+//     alert("Enter A valid user name")
+// }else{
+//     alert(a)
+// }
+
+
+
+Chapter No 26 TO 30
+
+
+Task No 1
+
+
+// var a = prompt("Enter Your Value");
+// var b = Math.round(a)
+// var c = Math.floor(a)
+// var d = Math.ceil(a)
+// document.write("The Value You Entered : " + a)
+// document.write("<br> It Is A Round Value : " + b)
+// document.write("<br>It Is A Floor Value : " + c)
+// document.write("<br>It Is A Ceil Value : " + d)
+
+
+
+TASK N0 2
+
+
+// var a = prompt("Enter Your negetive Value");
+// var b = Math.round(a)
+// var c = Math.floor(a)
+// var d = Math.ceil(a)
+// document.write("The Negetive Value You Entered : " + a)
+// document.write("<br> It Is A Round Value : " + b)
+// document.write("<br>It Is A Floor Value : " + c)
+// document.write("<br>It Is A Ceil Value : " + d)
+
+// dice vlue
+
+// var a = prompt("Enter Your Value")
+// var b = Math.random(a) * 6;
+// var c = Math.round(b)
+// document.write("Dice Value Is "+ c)
+
+
+
+
+
+random value of coin
+
+
+// var a = prompt("Enter The Name Of First Caller")
+// var b = prompt("Enter The Name Of second Caller")
+// var c = Math.random() * 2;
+// var d = Math.floor(c)
+
+// if (d === 0){
+//     document.write(" Heads " + a + " Win The Toss")
+// }else{
+//     document.write(" Tails " + b + " Win The Toss")
+// }
+
+
+
+
+weight showing
+
+
+// var a = prompt("Enter Your Weight In Kilograms")
+// var b = Math.random() * 100;
+// var c = Math.floor(b)
+// document.write("The Random Number Between 1 And 100 Is: " + c)
+
+
+
+secret number
+
+
+// var a = prompt("Enter The Secret Value")
+// if(a == 6){
+//     document.write("Wellcome To Our Page.")
+// }else{
+// alert("Sorry The Number You Have Entered Is Incorrect")
+// }
+
+
+ weight
+
+// var a = prompt("Please Enter Your Weight In kgs")
+// document.write(a + "Kgs")
+
+
+
+Chapter No 31 TO 34.
+
+
+// (1) Task NO ONE
+
+// var a = new Date()
+// document.write(a)
+
+
+
+ (2) Task NO TWO
+
+// var a = new Date()
+// var b = a.getMonth()
+// var c = (b + " June")
+// document.write(c)
+
+
+
+ (3) Task NO THREE
+
+// var a = new Date ()
+// var b = a.toString();
+// var c = b.slice(0,3)
+// document.write("Today Is " +c)
+
+
+
+
